91b52c5b60f762cd9d46ec2042a69e52e3746a1a
sadafgri <sadafgri696@gmail.com>
2023-11-21 13:20:47 +0330

    html code form showing data

---
+++ edit.php
@@ -0,0 +1,18 @@
+<html>
+<header>
+    <style>
+        body{
+            text-align: center;
+            background-color: aqua;
+            margin-top: 50px;
+        }
+    </style>
+</header>
+<body>
+<form action="my.php" method="post">
+    <label>Id:</label>
+    <input type="number" name="id2"><br><br>
+    <input type="submit" value="show data">
+</form>
+</body>
+</html>
