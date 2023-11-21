a7fcacee7b281673ee422afc6d9d351a53d5211c
sadafgri <sadafgri696@gmail.com>
2023-11-21 13:19:36 +0330

    html form code

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<html>
+<header>
+<style>
+    body{
+        text-align: center;
+        background-color: aqua;
+        margin-top: 50px;
+    }
+</style>
+</header>
+<body>
+<form action="home.php" method="post">
+    <label>Id:</label>
+    <input type="number" name="id"><br><br>
+    <label>first name:</label>
+    <input type="text" name="firstname"><br><br>
+    <label>last name:</label>
+    <input type="text" name="lastname"><br><br>
+    <label>phone number:</label>
+    <input type="number" name="phonenumber"><br><br>
+    <label>your age:</label>
+    <input type="number" name="age"><br><br>
+    <label>your password:</label>
+    <input type="number" name="pass"><br><br>
+    <label>choose a gender:</label>
+    <input type="text" name="gender"><br><br>
+    <input type="submit">
+</form>
+</body>
+</html>
+
